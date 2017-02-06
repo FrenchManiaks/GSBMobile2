c8a9a58ebcaa6fa168fab6e017c391418126f830
darkfusion69 <matias.hidalgo@hotmail.fr>
2017-02-06 14:12:52 +0100

    test

--- app/src/main/java/fr/falconteam/www/projetgsb/HomeFragment.java
+++ app/src/main/java/fr/falconteam/www/projetgsb/HomeFragment.java
@@ -25,8 +25,33 @@ public class HomeFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TabHost host = (TabHost)view.findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Tab Two");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Tab Three");
+        host.addTab(spec);
+
+        return view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+       // return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
