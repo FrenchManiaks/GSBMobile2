dc37a08a97ff29c90093216e98eb3eca268d9532
darkfusion69 <matias.hidalgo@hotmail.fr>
2017-02-06 16:22:58 +0100

    TabHost Fonctionnel

--- app/src/main/java/fr/falconteam/www/projetgsb/HomeFragment.java
+++ app/src/main/java/fr/falconteam/www/projetgsb/HomeFragment.java
@@ -4,9 +4,11 @@ package fr.falconteam.www.projetgsb;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TabHost;
 
 
@@ -26,32 +28,32 @@ public class HomeFragment extends Fragment  {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        //View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        TabHost host = (TabHost)getView().findViewById(R.id.tabHost);
+        TabHost host = (TabHost)view.findViewById(R.id.tabHost);
         host.setup();
 
         //Tab 1
         TabHost.TabSpec spec = host.newTabSpec("Tab One");
         spec.setContent(R.id.tab1);
-        spec.setIndicator("Tab One");
+        spec.setIndicator("Activités");
         host.addTab(spec);
 
         //Tab 2
         spec = host.newTabSpec("Tab Two");
         spec.setContent(R.id.tab2);
-        spec.setIndicator("Tab Two");
+        spec.setIndicator("P. Auto");
         host.addTab(spec);
 
         //Tab 3
         spec = host.newTabSpec("Tab Three");
         spec.setContent(R.id.tab3);
-        spec.setIndicator("Tab Three");
+        spec.setIndicator("RDV Perso");
         host.addTab(spec);
 
-        //return view;
+        return view;
         // Inflate the layout for this fragment
-       return inflater.inflate(R.layout.fragment_home, container, false);
+       //return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
